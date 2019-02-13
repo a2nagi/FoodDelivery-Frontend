d5bfc87ebfbebb63c3e28f8432d093e9c90b5402
Nagi <a2nagi@uwaterloo.ca>
2019-02-13 12:19:25 +0900

    Add own component to use

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person.js'
+// Home to the root component
 
 class App extends Component {
   render() {
-      // <div className="App">
-      //   <h1> Hi I'm a react app!</h1>
-      // </div>
+    return(
+      <div className="App">
+        <h1> Hi I'm a react app!</h1>
+        <p>This is really Working!</p>
+        <Person />
+      </div>
     
     // The way we use React.createElement below is the exact same as the html above. In fact the above gets compiled 
     // to execute the below called function.
-    return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'));
+    // return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'));
 
+    )
   }
 }
 
---
+++ react-guide/src/Person/Person.js
@@ -0,0 +1,11 @@
+// We want to create a component
+
+import React from 'react';
+
+const person = () => {
+    // As we said we are creating a component, it is a function that simply outputs JSX
+    return <p>I'm a person!</p>   
+}
+
+// Exporting the constant (person) we just created.
+export default person;
