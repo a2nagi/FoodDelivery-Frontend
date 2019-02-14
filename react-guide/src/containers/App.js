41739b6c70f88123e6292ab0bc34e21a4e643d72
Nagi <a2nagi@uwaterloo.ca>
2019-02-14 10:53:58 +0900

    Decouple components and states from app.js

--- react-guide/src/containers/App.js
+++ react-guide/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import Person from './Person/Person.js'
+import Persons from '../components/Persons/Persons.js'
+import Cockpit from '../components/Cockpit/Cockpit.js'
 // Home to the root component
 
 class App extends Component {
@@ -28,12 +28,17 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons})
+    this.setState({persons: persons});
   }
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = this.state.persons[personIndex]
     this.setState({
       persons: [
       { name: 'Max', age: 28 },
@@ -49,47 +54,25 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'White',
-      font: 'inherit',
-      border: '1x solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
     if (this.state.showPersons){
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-                    click={() => this.deletePersonHandler(index)}
-                    name={person.name} 
-                    age={person.age}/>
-          })}
-
-          {/* <Person 
-            name ={this.state.persons[0].name} 
-            age={this.state.persons[0].age}/>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age} 
-            click={this.switchNameHandler.bind(this, 'Max!')}
-            changed={this.nameChangeHandler}>My Hobbies: Racing </Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/> */}
-        </div> 
+            <Persons 
+              persons = {this.state.persons}
+              clicked = {this.deletePersonHandler}
+              changed={this.nameChangeHandler} />
       )
     }
 
     return(
       <div className="App">
-        <h1> Hi I'm a react app!</h1>
-        <p>This is really Working!</p>
-        <button style = {style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <Cockpit 
+          showPersons={this.state.showPersons} 
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler} />
         {persons}
       </div>
+
     
     // The way we use React.createElement below is the exact same as the html above. In fact the above gets compiled 
     // to execute the below called function.
